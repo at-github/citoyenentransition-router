da1bedb1dc4ede300529e8b530930fcda2aa14fb
Tarik Amar <tarik.amar@gmail.com>
2022-11-23 17:14:24 +0100

    Server - Serve file when is exist

--- server.rb
+++ server.rb
@@ -1,4 +1,60 @@
 require 'socket'
+require 'uri'
+
+# Files will be served from this directory
+WEB_ROOT = './public'
+
+# Map extensions to their content type
+CONTENT_TYPE_MAPPING = {
+  'markdown' => 'text/markdown',
+  'html' => 'text/html',
+  'txt' => 'text/plain',
+  'png' => 'image/png',
+  'jpg' => 'image/jpeg'
+}
+
+# Treat as binary data if content type cannot be found
+DEFAULT_CONTENT_TYPE = 'application/octet-stream'
+
+# This helper function parses the extension of the
+# requested file and then looks up its content type.
+def content_type(path)
+  ext = File.extname(path).split(".").last
+  CONTENT_TYPE_MAPPING.fetch(ext, DEFAULT_CONTENT_TYPE)
+end
+
+# This helper function parses the Request-Line and
+# generates a path to a file on the server.
+# Takes a request line (e.g. "GET /path?foo=bar HTTP/1.1")
+# and extracts the path from it, scrubbing out parameters
+# and unescaping URI-encoding.
+#
+# This cleaned up path (e.g. "/path") is then converted into
+# a relative path to a file in the server's public folder
+# by joining it with the WEB_ROOT.
+def requested_file(request_line)
+  request_uri  = request_line.split(" ")[1]
+  path         = URI.unescape(URI(request_uri).path)
+
+  clean = []
+
+  # Split the path into components
+  parts = path.split("/")
+
+  parts.each do |part|
+    # skip any empty or current directory (".") path components
+    next if part.empty? || part == '.'
+    # If the path component goes up one directory level (".."),
+    # remove the last clean component.
+    # Otherwise, add the component to the Array of clean components
+    part == '..' ? clean.pop : clean << part
+
+    # return the web root joined to the clean path
+    File.join(WEB_ROOT, *clean)
+  end
+
+  File.join(WEB_ROOT, path)
+end
 
 # Initialize a TCPServer object that will listen
 # on localhost:2345 for incoming connections.
@@ -11,31 +67,38 @@ loop do
   # that can be used in a similar fashion to other Ruby
   # I/O objects. (In fact, TCPSocket is a subclass of IO.)
   socket = server.accept
+  request_line = socket.gets
 
-  # Read the first line of the request (the Request-Line)
-  request = socket.gets
+  path = requested_file(request_line)
+  + path = File.join(path, 'index.txt') if File.directory?(path)
 
-  # Log the request to the console for debugging
-  STDERR.puts request
+  # Make sure the file exists and is not a directory
+  # before attempting to open it.
+  if File.exist?(path.to_s) && !File.directory?(path)
+    File.open(path, "rb") do |file|
+      socket.print "HTTP/1.1 200 OK\r\n" +
+                   "Content-Type: #{content_type(file)}\r\n" +
+                   "Content-Length: #{file.size}\r\n" +
+                   "Connection: close\r\n"
 
+      socket.print "\r\n"
 
-  response = "Citoyen en transition\n"
+      # write the contents of the file to the socket
+      IO.copy_stream(file, socket)
+    end
+  else
+    message = "File not found\n"
 
-  # We need to include the Content-Type and Content-Length headers
-  # to let the client know the size and type of data
-  # contained in the response. Note that HTTP is whitespace
-  # sensitive, and expects each header line to end with CRLF (i.e. "\r\n")
-  socket.print "HTTP/1.1 200 OK\r\n" +
-               "Content-Type: text/plain\r\n" +
-               "Content-Length: #{response.bytesize}\r\n" +
-               "Connection: close\r\n"
+    # respond with a 404 error code to indicate the file does not exist
+    socket.print "HTTP/1.1 404 Not Found\r\n" +
+                 "Content-Type: text/plain\r\n" +
+                 "Content-Length: #{message.size}\r\n" +
+                 "Connection: close\r\n"
 
-  # Print a blank line to separate the header from the response body,
-  # as required by the protocol.
-  socket.print "\r\n"
+    socket.print "\r\n"
 
-  # Print the actual response body, which is just "Hello World!\n"
-  socket.print response
+    socket.print message
+  end
 
   # Close the socket, terminating the connection
   socket.close
