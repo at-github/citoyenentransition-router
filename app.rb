7a48d6f1943a353205143758ce656c03ed74f408
Tarik Amar <tarik.amar@gmail.com>
2022-12-04 22:13:17 +0000

    Remove unnecessary method

--- app.rb
+++ app.rb
@@ -23,32 +23,6 @@ content_folder = 'content/' + config['content_folder']
 abort 'Wrong path for content_folder key'\
   ' in config.yml file' if !File.directory? content_folder
 
-# This helper function parses the Request-Line and
-# generates a path to a file on the server.
-# Takes a request line (e.g. "GET /path?foo=bar HTTP/1.1")
-# and extracts the path from it, scrubbing out parameters
-# and unescaping URI-encoding.
-#
-# This cleaned up path (e.g. "/path") is then converted into
-# a relative path to a file in the server's
-def requested_file(request)
-  clean = []
-
-  # Split the path into components
-  parts = request.split("/")
-
-  parts.each do |part|
-    # skip any empty or current directory (".") path components
-    next if part.empty? || part == '.'
-    # If the path component goes up one directory level (".."),
-    # remove the last clean component.
-    # Otherwise, add the component to the Array of clean components
-    part == '..' ? clean.pop : clean << part
-  end
-
-  request
-end
-
 myServer = Server.new
 translation = Translation.new(config['translations'])
 content = Content.new(content_folder, translation)
@@ -56,13 +30,8 @@ content = Content.new(content_folder, translation)
 render = Render.new(@title, @links)
 
 STDOUT.puts 'Server started localhost:2345'
-# loop infinitely, processing one incoming
-# connection at a time.
 loop do
-  # Wait until a client connects, then return a TCPSocket
-  # that can be used in a similar fashion to other Ruby
-  # I/O objects. (In fact, TCPSocket is a subclass of IO.)
-  path = requested_file(myServer.request())
+  path = myServer.request()
 
   # Statics
   if (path.match? 'favicon.ico') || (/^\/public.*$/.match?(path) == true)
