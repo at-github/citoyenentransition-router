4939fa889e957b6c27f5ace0f8be6994861e5ba1
Tarik Amar <tarik.amar@gmail.com>
2022-12-05 22:06:41 +0000

    Remove unnecessaries ()

--- app.rb
+++ app.rb
@@ -9,11 +9,11 @@ require_relative 'src/services/Server'
 require_relative 'src/services/Translation'
 
 # Checks
-pwd  = File.dirname(File.expand_path(__FILE__))
+pwd  = File.dirname File.expand_path __FILE__
 abort 'You must create content folder'      if !File.directory? "#{pwd}/content"
 abort 'You must create a "config.yml" file' if !File.exist? "#{pwd}/config.yml"
 
-config = YAML.load_file("#{pwd}/config.yml")
+config = YAML.load_file "#{pwd}/config.yml"
 abort 'You must create a "title" key in config.yml file,'\
   ' with a correct path' if !config.key? "title"
 title = config['title']
@@ -26,17 +26,13 @@ abort 'Wrong path for content_folder key'\
   ' in config.yml file' if !File.directory? content_folder
 
 server = Server.new
-translation = Translation.new(config['translations'])
-content = Content.new(content_folder, translation)
-render = Render.new(pwd, title, content.get_links())
-dispatcher = Dispatcher.new(
-  server,
-  render,
-  content
-)
+translation = Translation.new config['translations']
+content = Content.new content_folder, translation
+render = Render.new pwd, title, content.get_links
+dispatcher = Dispatcher.new server, render, content
 
 STDOUT.puts 'Server started localhost:2345'
 loop do
-  path = server.request()
-  dispatcher.dispatch(path)
+  path = server.request
+  dispatcher.dispatch path
 end
