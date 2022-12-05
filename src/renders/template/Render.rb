fd8480efcade6232974763d57510831c3ff41bb1
Tarik Amar <tarik.amar@gmail.com>
2022-12-05 22:06:41 +0000

    Remove unnecessaries () on Render

--- src/renders/template/Render.rb
+++ src/renders/template/Render.rb
@@ -3,11 +3,11 @@ class Render
     @title = title.capitalize
     @links = links
 
-    @layout_template    = ERB.new(File.read("#{root}/src/templates/layout.erb"))
-    @home_template      = ERB.new(File.read("#{root}/src/templates/home.erb"))
-    @archive_template   = ERB.new(File.read("#{root}/src/templates/archive.erb"))
-    @page_template      = ERB.new(File.read("#{root}/src/templates/page.erb"))
-    @not_found_template = ERB.new(File.read("#{root}/src/templates/not_found.erb"))
+    @layout_template    = ERB.new File.read "#{root}/src/templates/layout.erb"
+    @home_template      = ERB.new File.read "#{root}/src/templates/home.erb"
+    @archive_template   = ERB.new File.read "#{root}/src/templates/archive.erb"
+    @page_template      = ERB.new File.read "#{root}/src/templates/page.erb"
+    @not_found_template = ERB.new File.read "#{root}/src/templates/not_found.erb"
   end
 
   def render(content, title = nil)
@@ -21,35 +21,28 @@ class Render
   end
 
   def render_home(content)
-    return render(@home_template.result_with_hash(
-        content: content
-    ))
+    return render @home_template.result_with_hash content: content
   end
 
   def render_archive(content, title)
-    title = title.gsub('/', '')
-    return render(
-      @archive_template.result_with_hash(
-        content: content,
-        title: title
-      ),
-      title
-    )
+    title = title.gsub '/', ''
+    return render @archive_template.result_with_hash(
+      content: content,
+      title: title
+    ), title
   end
 
   def render_page(content, title, slug)
     category = /^\/(?<category>.*)\/.*$/.match(slug)['category']
 
-    return render(
-      @page_template.result_with_hash(
-        content: content,
-        category: category
-      ),
-      title
-    )
+    return render @page_template.result_with_hash(
+      content: content,
+      category: category
+    ), title
+
   end
 
-  def render_404()
-    return render(@not_found_template.result())
+  def render_404
+    return render @not_found_template.result
   end
 end
